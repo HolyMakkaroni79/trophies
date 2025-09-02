cc11aa011ac850e0d863a960c80422b69df2b81d
Toni Kordik <Toni.Kordik@gmail.com>
2025-09-02 17:53:34 +0200

    Initial commit

---
+++ src/main/java/me/HolyMakkaroni79/trophies/Trophies.java
@@ -0,0 +1,17 @@
+package me.HolyMakkaroni79.trophies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Trophies extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
